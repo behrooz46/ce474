96addf9a03f0d3c181af2814baf5eefdb7df09d3
minmit <mina.tahmasbi@gmail.com>
2014-06-29 12:43:36 +0430

    certificate

--- src/ca/SignServer.java
+++ src/ca/SignServer.java
@@ -1,10 +1,15 @@
 package ca;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
@@ -14,15 +19,64 @@ import java.security.cert.CertificateFactory;
 import sun.security.x509.*;
 
 import java.security.cert.*;
+import java.security.interfaces.RSAPrivateCrtKey;
 import java.security.*;
 import java.math.BigInteger;
+import java.util.Calendar;
 import java.util.Date;
+
+import javax.security.auth.x500.X500Principal;
+
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.DERBMPString;
+import org.bouncycastle.asn1.DERBitString;
+import org.bouncycastle.asn1.DERInteger;
+import org.bouncycastle.asn1.DERNull;
+import org.bouncycastle.asn1.DERObjectIdentifier;
+import org.bouncycastle.asn1.DEROutputStream;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.DigestInfo;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.asn1.x509.TBSCertificateStructure;
+import org.bouncycastle.asn1.x509.Time;
+import org.bouncycastle.asn1.x509.V3TBSCertificateGenerator;
+import org.bouncycastle.asn1.x509.X509CertificateStructure;
+import org.bouncycastle.asn1.x509.X509Extensions;
+import org.bouncycastle.asn1.x509.X509Name;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
+import org.bouncycastle.crypto.AsymmetricBlockCipher;
+import org.bouncycastle.crypto.CipherParameters;
+import org.bouncycastle.crypto.digests.SHA1Digest;
+import org.bouncycastle.crypto.encodings.PKCS1Encoding;
+import org.bouncycastle.crypto.engines.RSAEngine;
+import org.bouncycastle.crypto.params.RSAPrivateCrtKeyParameters;
+import org.bouncycastle.jce.PrincipalUtil;
+import org.bouncycastle.jce.interfaces.PKCS12BagAttributeCarrier;
+import org.bouncycastle.jce.provider.X509CertificateObject;
+import org.bouncycastle.openssl.PEMWriter;
+import org.bouncycastle.util.Strings;
+//import org.bouncycastle.x509.X509Util;
+import org.bouncycastle.x509.X509V1CertificateGenerator;
+import org.bouncycastle.x509.X509V3CertificateGenerator;
+import org.bouncycastle.x509.extension.AuthorityKeyIdentifierStructure;
+import org.bouncycastle.x509.extension.SubjectKeyIdentifierStructure;
 
 
 public class SignServer extends Thread {
 
 	private ServerSocket serverSocket;
+	static private X509Certificate caCert;
+	static private KeyPair caPair;
+//	private String caFile, caPassword, caAlias;
+//	private RSAPrivateCrtKeyParameters caPrivateKey;
+//	private String exportPassword, exportFile;
 
+	
 	public SignServer(int port) throws IOException {
 		serverSocket = new ServerSocket(port);
 		//		serverSocket.setSoTimeout(10000);
@@ -95,6 +149,124 @@ public class SignServer extends Thread {
 	  cert = new X509CertImpl(info);
 	  cert.sign(privkey, algorithm);
 	  return cert;
-	}   
+	}
+			
+	
+	@SuppressWarnings("deprecation")
+	static private X509Certificate generateSelfSignedX509Certificate() throws Exception {
+
+
+	    // yesterday
+	    Date validityBeginDate = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);
+	    // in 2 years
+	    Date validityEndDate = new Date(System.currentTimeMillis() + 2 * 365 * 24 * 60 * 60 * 1000);
+
+	    // GENERATE THE PUBLIC/PRIVATE RSA KEY PAIR
+	    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+	    keyPairGenerator.initialize(1024, new SecureRandom());
+
+	    KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+	    // GENERATE THE X509 CERTIFICATE
+	    X509V1CertificateGenerator certGen = new X509V1CertificateGenerator();
+	    X500Principal dnName = new X500Principal("CN=John Doe");
+
+	    certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
+	    certGen.setSubjectDN(dnName);
+	    certGen.setIssuerDN(dnName); // use the same
+	    certGen.setNotBefore(validityBeginDate);
+	    certGen.setNotAfter(validityEndDate);
+	    certGen.setPublicKey(keyPair.getPublic());
+	    certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+
+	    X509Certificate cert = certGen.generate(keyPair.getPrivate(), "BC");
+
+	    // DUMP CERTIFICATE AND KEY PAIR
+
+//	    printCert(cert);
+	    	    
+	    caCert = cert;
+	    caPair = keyPair;
+	    return cert;
+	}
+	
+	static private void createCert() throws NoSuchAlgorithmException, NoSuchProviderException{
+		Date startDate = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000);                // time from which certificate is valid
+		Date expiryDate = new Date(System.currentTimeMillis() + 1 * 365 * 24 * 60 * 60 * 1000);               // time after which certificate is not valid
+		BigInteger serialNumber = BigInteger.valueOf(System.currentTimeMillis());       // serial number for certificate
+		PrivateKey caKey = caPair.getPrivate();              // private key of the certifying authority (ca) certificate
+		
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", "BC");
+	    keyPairGenerator.initialize(1024, new SecureRandom());
+
+	    KeyPair keyPair = keyPairGenerator.generateKeyPair();
+	    
+
+		X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
+		X500Principal              subjectName = new X500Principal("CN=Test V3 Certificate");
+		 
+		certGen.setSerialNumber(serialNumber);
+		certGen.setIssuerDN(caCert.getSubjectX500Principal());
+		certGen.setNotBefore(startDate);
+		certGen.setNotAfter(expiryDate);
+		certGen.setSubjectDN(subjectName);
+		certGen.setPublicKey(keyPair.getPublic());
+		certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+		
+		try{
+			certGen.addExtension(X509Extensions.AuthorityKeyIdentifier, false,
+			                        new AuthorityKeyIdentifierStructure(caCert));
+			certGen.addExtension(X509Extensions.SubjectKeyIdentifier, false,
+			                        new SubjectKeyIdentifierStructure(keyPair.getPublic()));
+			 
+			X509Certificate cert = certGen.generate(caKey, "BC");   // note: private key of CA
+			printCert(cert);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		try {
+			Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+			generateSelfSignedX509Certificate();
+			createCert();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static private void printCert(X509Certificate cert){
+		System.out.println(new String(new char[80]).replace("\0", "="));
+	    System.out.println("CERTIFICATE TO_STRING");
+	    System.out.println(new String(new char[80]).replace("\0", "="));
+	    System.out.println();
+	    System.out.println(cert);
+	    System.out.println();
+
+	    System.out.println(new String(new char[80]).replace("\0", "="));
+	    System.out.println("CERTIFICATE PEM (to store in a cert-johndoe.pem file)");
+	    System.out.println(new String(new char[80]).replace("\0", "="));
+	    System.out.println();
+	    try{
+		    PEMWriter pemWriter = new PEMWriter(new PrintWriter(System.out));
+		    pemWriter.writeObject(cert);
+		    pemWriter.flush();
+		    System.out.println();
+	    }
+	    catch(Exception e){
+	    	
+	    }
+
+//	    System.out.println(new String(new char[80]).replace("\0", "="));
+//	    System.out.println("PRIVATE KEY PEM (to store in a priv-johndoe.pem file)");
+//	    System.out.println(new String(new char[80]).replace("\0", "="));
+//	    System.out.println();
+//	    pemWriter.writeObject(keyPair.getPrivate());
+//	    pemWriter.flush();
+//	    System.out.println();
+	}
 	
 }
